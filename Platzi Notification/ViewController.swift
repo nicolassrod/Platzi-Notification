af27626eee0643a022c8285c802b32214c815865
Nicolas Rod <nrq5510@gmail.com>
2017-12-20 18:14:19 -0500

    Initial Commit

---
+++ Platzi Notification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Platzi Notification
+//
+//  Created by Nicolás Rodríguez on 20/12/17.
+//  Copyright © 2017 Nicolás Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Platzi NotificationTests/Platzi_NotificationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Platzi_NotificationTests.swift
+//  Platzi NotificationTests
+//
+//  Created by Nicolás Rodríguez on 20/12/17.
+//  Copyright © 2017 Nicolás Rodríguez. All rights reserved.
+//
+
+import XCTest
+@testable import Platzi_Notification
+
+class Platzi_NotificationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
